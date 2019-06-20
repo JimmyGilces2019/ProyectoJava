fe01d05483b6a38727c653f0688f5f121c67718f
Jimmy <jgilces@espol.edu.ec>
2019-06-20 15:11:45 -0500

    Instanciacion de la clase ventana

--- JavaApplication39/src/javaapplication39/JavaApplication39.java
+++ JavaApplication39/src/javaapplication39/JavaApplication39.java
@@ -15,7 +15,7 @@ public class JavaApplication39 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        JavaApplication39 vent = new JavaApplication39();
     }
     
 }
